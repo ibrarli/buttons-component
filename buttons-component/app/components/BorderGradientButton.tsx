151974518e9eba9e9d54e781249a3b86d75e14c2
Ibrar-Haidar <106898965+Ibrar-Haidar@users.noreply.github.com>
2025-05-17 11:44:44 +0500

    Added ShinyHoverButton

--- buttons-component/app/components/BorderGradientButton.tsx
+++ buttons-component/app/components/BorderGradientButton.tsx
@@ -1,7 +1,7 @@
 export default function BorderGradientButton() {
   return (
     <div>
-      <div className="flex items-center justify-center h-screen">
+      <div >
         <div className="relative group">
           <button className="relative inline-block p-px font-semibold leading-6 text-white bg-gray-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95">
             <span className="absolute inset-0 rounded-xl bg-gradient-to-r from-teal-400 via-blue-500 to-purple-500 p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
@@ -20,7 +20,7 @@ export default function BorderGradientButton() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                     d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
                     fill-rule="evenodd"
                   ></path>
--- buttons-component/app/page.tsx
+++ buttons-component/app/page.tsx
@@ -1,9 +1,15 @@
 import BorderGradientButton from "./components/BorderGradientButton";
+import ShinyHoverButton from "./components/ShinyHoverButton";
 
 export default function Home() {
   return (
-    <div>
-      <BorderGradientButton />
+    <div className="flex">
+      <div className="m-5"> 1.
+        <ShinyHoverButton />
+      </div>
+      <div className="m-5"> 2.
+        <BorderGradientButton />
+      </div>
     </div>
   );
 }
